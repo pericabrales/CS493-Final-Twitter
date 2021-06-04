dd41baf059aa4c65fcfb8230d659445b4e0cfccf
Peri <cabralep@oregonstate.edu>
2021-06-03 23:24:14 -0700

    could have 2 routes done. Need tweets to test

---
+++ models/favorite.js
@@ -0,0 +1,59 @@
+const User = require("../models/user");
+const Tweet = require("../models/tweet");
+
+//to get the array of the user's favorites
+async function getAllUsersFaves(id, page){
+    //get all of the user's info
+    const user = await User.findById(id);
+    console.log("user: ", user);
+
+    //check to make sure the favorites isn't null
+    if(user.favorites.count){
+        console.log("user favorites: ", user.favorites);
+        //need pagination (just the page thing)
+        console.log("favorites count", user.favorites.count);
+        const count = user.favorites.count;
+        const pageSize = 10;
+        const lastPage = Math.ceil(count / pageSize);
+        page = page > lastPage ? lastPage : page;
+        page = page < 1 ? 1 : page;
+        const offset = (page - 1) * pageSize;
+        const arraySplit = user.favorites.slice(offset, (page*pageSize));
+
+        return{
+            favorites: arraySplit,
+            page: page,
+            totalPages: lastPage,
+            pageSize: pageSize,
+            count: count
+        };
+    }
+    //if its an empty array, just send back 0s and stuff to make it not look gross
+    else{
+        return{
+            favorites: [],
+            page: 0,
+            totalPages: 0,
+            pageSize: 0,
+            count: 0
+        };
+    }
+}
+exports.getAllUsersFaves = getAllUsersFaves;
+
+//to insert a tweet into the favorites array
+async function insertTweetToFave(userid, tweetid){
+    //get the user by id
+    const user = await User.findById(userid);
+    //get the tweet by id
+    const tweet = await Tweet.findById(tweetid);
+
+    //insert the tweet into the the user's favorites array
+    user.favorites.push(tweet);
+
+    //get the index that the tweet was just pushed into (will be the last index always)
+    const indexNum = user.favorites.count - 1;
+
+    return indexNum;
+}
+exports.insertTweetToFave = insertTweetToFave;
--- routes/favorites.js
+++ routes/favorites.js
@@ -1,8 +1,78 @@
 const express = require("express");
 const router = express.Router();
+const passport = require('passport');
+const { getAllUsersFaves, insertTweetToFave } = require("../models/favorite");
 
-router.get("/", (req, res) => {
-  res.send("Favorites");
+
+//get request for listing a user's favorite tweets (should be in an array)
+router.get("/", passport.authenticate('local'), async (req, res) => {
+  console.log("user id: ", req.user.id);
+  try{
+    //get the favorites of the user also with pagination
+    const faves = await getAllUsersFaves(req.user.id, (req.query.page || 1));
+    console.log("after getAllUsersFaves call");
+    //now make pagination also show up
+    faves.links = {};
+    if(faves.page < faves.totalPages){
+      faves.links.nextPage = `/favorites?page=${faves.page+1}`;
+      faves.links.lastPage = `/favorites?page=${faves.totalPages}`;
+    }
+    if(faves.page > 1){
+      faves.links.prevPage = `/favorites?page=${faves.page-1}`;
+      faves.links.firstpage = `/favorites?page=1`;
+    }
+    res.status(200).send(faves);
+  }catch(err){
+    console.error(err);
+    res.status(500).send({
+      error:"Error fetching favorites. Try again later"
+    });
+  }
+});
+
+//get request for getting a specific favorited tweet
+router.get('/:tweetid', passport.authenticate('local'), async (req, res) => {
+
+});
+
+
+//Post request for favoriting a tweet by the tweet id
+//should also update the tweet's favorites array (I think)
+//authenticate needs a json of the username and password sent to it
+router.post('/:tweetid', passport.authenticate('local'), async (req, res) => {
+  console.log("user id: ", req.user.id);
+  try{
+    //insert the new tweet to favorite array
+    //get the array index number back
+    const faveIndex = await insertTweetToFave(req.user.id, req.params.tweetid);
+    res.status(201).send({
+      favoritesIndex: faveIndex,
+      links: {
+        favoriteTweet: `/favorites/${faveIndex}`
+      }
+    });
+  }
+  catch(err){
+    console.error(err);
+    res.status(500).send({
+      error:"That is not a valid tweet id. Please try again."
+    });
+  }
+});
+
+//delete request for unfavoriting a tweet by id
+router.delete('/:tweetid', passport.authenticate('local'), async (req, res) => {
+  console.log("user id: ", req.user.id);
+  try{
+    //delete tweet from favorite array
+
+  }
+  catch(err){
+    console.error(err);
+    res.status(500).send({
+      error:"That is not a valid tweet id. Please try again."
+    });
+  }
 });
 
 module.exports = router;
