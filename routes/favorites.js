9eabb60f48f61021a27c9bb64d8b85a543141c02
paynem <mpayne677@gmail.com>
2021-06-06 15:47:42 -0700

    Merged everything, little fixes

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,8 +1,77 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const User = require("../models/user");
+const Tweet = require("../models/tweet");
+const { getAllUsersFaves, insertTweetToFave } = require("../models/favorite");
+const catchAsync = require("../utils/catchAsync");
+const { isLoggedInReject, isLoggedInAccept } = require("../middleware");
 
-router.get("/", (req, res) => {
-  res.send("Favorites");
-});
+//get request for listing a user's favorite tweets (should be in an array)
+router.get(
+	"/",
+	isLoggedInReject,
+	catchAsync(async (req, res) => {
+		console.log("user id: ", req.user.id);
+		//get the favorites of the user also with pagination
+		const faves = await getAllUsersFaves(req.user.id, req.query.page || 1);
+		console.log("after getAllUsersFaves call");
+		//links for the pagination of the favorites
+		faves.links = {};
+		if (faves.page < faves.totalPages) {
+			faves.links.nextPage = `/favorites?page=${faves.page + 1}`;
+			faves.links.lastPage = `/favorites?page=${faves.totalPages}`;
+		}
+		if (faves.page > 1) {
+			faves.links.prevPage = `/favorites?page=${faves.page - 1}`;
+			faves.links.firstpage = `/favorites?page=1`;
+		}
+		res.status(200).send(faves);
+	})
+);
+
+//Post request for favoriting a tweet by the tweet id
+//should also update the tweet's favorites array (I think)
+router.post(
+	"/:tweetid",
+	isLoggedInReject,
+	catchAsync(async (req, res) => {
+		console.log("user id: ", req.user.id);
+		//insert the new tweet to favorite array
+		await User.updateOne(
+			{ _id: req.user.id },
+			{ $push: { favorites: req.params.tweetid } }
+		);
+
+		//just sending back the link to the tweet that they favorited in case they want to see it again
+		res.status(200).send({
+			links: {
+				favoriteTweet: `/tweets/${req.params.tweetid}`,
+			},
+		});
+	})
+);
+
+//delete request for unfavoriting a tweet by id
+router.delete(
+	"/:tweetid",
+	isLoggedInReject,
+	catchAsync(async (req, res) => {
+		console.log("user id: ", req.user.id);
+
+		//delete tweet from favorite array
+		await User.updateOne(
+			{ _id: req.user.id },
+			{ $pull: { favorites: req.params.tweetid } }
+		);
+
+		res.status(204).end();
+	})
+);
+
+//get request for getting a specific favorited tweet
+// router.get('/:tweetid', isLoggedInReject, catchAsync(async(req, res) => {
+
+// }));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -19,6 +19,25 @@ const getUserInfo = async (id) => {
 	return user;
 };
 
+const getDmsPage = async (page, id) => {
+	const user = await User.findById(id).populate("DMs");
+	const count = user.DMs.length;
+	const pageSize = 10;
+	const lastPage = Math.ceil(count / pageSize);
+	page = page > lastPage ? lastPage : page;
+	page = page < 1 ? 1 : page;
+	const offset = (page - 1) * pageSize;
+
+	return {
+		dms: user.DMs.slice(offset, offset + pageSize),
+		page: page,
+		totalPages: lastPage,
+		pageSize: pageSize,
+		count: count,
+	};
+	// return {shit: "shit"}
+};
+
 router.get("/", isLoggedInReject, (req, res) => {
 	res.send("testing logged in functions");
 });
@@ -45,26 +64,30 @@ router.post("/login", passport.authenticate("local"), (req, res) => {
 	res.send("Logged in successfully!");
 });
 
-router.get("/:id", isLoggedInAccept, async (req, res, next) => {
-	if (req.logged === true && req.user._id.toString() === req.params.id) {
-		const results = await getUserInfo(req.params.id);
-		if (!results) {
-			return next();
+router.get(
+	"/:id",
+	isLoggedInAccept,
+	catchAsync(async (req, res, next) => {
+		if (req.logged === true && req.user._id.toString() === req.params.id) {
+			const results = await getUserInfo(req.params.id);
+			if (!results) {
+				return next();
+			}
+			res.status(200).json(results);
+		} else {
+			const results = await getUserInfo(req.params.id);
+			res.status(200).json({
+				FirstName: results.firstName,
+				LastName: results.lastName,
+				birth_date: results.birth_date,
+				images: results.images,
+				favorites: results.favorites,
+				following: results.following,
+				followers: results.followers,
+			});
 		}
-		res.status(200).json(results);
-	} else {
-		const results = await getUserInfo(req.params.id);
-		res.status(200).json({
-			FirstName: results.firstName,
-			LastName: results.lastName,
-			birth_date: results.birth_date,
-			images: results.images,
-			favorites: results.favorites,
-			following: results.following,
-			followers: results.followers,
-		});
-	}
-});
+	})
+);
 
 router.patch(
 	"/:id",
@@ -93,6 +116,19 @@ router.get(
 	catchAsync(async (req, res, next) => {
 		const user = await User.findById(req.params.id).populate("images");
 		res.status(200).json(user.images);
+	})
+);
+
+router.get(
+	"/:id/dms",
+	catchAsync(async (req, res, next) => {
+		console.log(req.params.id);
+		const dmsPage = await getDmsPage(
+			parseInt(req.query.page) || 1,
+			req.params.id
+		);
+		console.log(dmsPage);
+		res.status(200).json(dmsPage);
 	})
 );
 
