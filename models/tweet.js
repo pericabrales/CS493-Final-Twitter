858f33218265ff4d57eef07fd564320ed216fc75
paynem <mpayne677@gmail.com>
2021-06-06 12:55:28 -0700

    basic tweet endpoints

--- models/tweet.js
+++ models/tweet.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tweetSchema = new mongoose.Schema({
+	userid: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
 	message: {
 		type: String,
 		required: true,
