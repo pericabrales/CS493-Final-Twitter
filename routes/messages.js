cc3b35afd115bf9b0f3df9ddc7d399e4c710c30e
paynem <mpayne677@gmail.com>
2021-06-06 12:43:20 -0700

    Made some adjustments

--- routes/messages.js
+++ routes/messages.js
@@ -4,49 +4,68 @@ const mongoose = require("mongoose");
 const User = require("../models/user");
 const DM = require("../models/dm");
 const catchAsync = require("../utils/catchAsync");
+const { isLoggedInReject, isLoggedInAccept } = require("../middleware");
 
 router.get("/", (req, res) => {
   res.send("Messages");
 });
 
+//This creates a new dm/conversation between two users.  We should probably check to  make sure one doesn't already exist
 router.post(
-  "/",
-  catchAsync(async (req, res) => {
-    const user1 = await User.findOne({ _id: req.body.userId1 });
-    const user2 = await User.findOne({ _id: req.body.userId2 });
+  "/", isLoggedInReject, catchAsync(
+  async (req, res) => {
 
     const dm = new DM({
-      userid1: user1._id,
-      userid2: user2._id,
-      messages: [
-        {
-          senderid: user1._id,
-          message: req.body.message,
-          date: req.body.date,
-        },
-      ],
-    });
-    await User.updateOne({ _id: req.body.userId1 }, { $push: { DMs: dm } });
-    await User.updateOne({ _id: req.body.userId2 }, { $push: { DMs: dm } });
+			userid1: req.user._id,
+			userid2: req.body.userId,
+			messages: [
+				{
+					senderid: req.user._id,
+					message: req.body.message,
+					date: req.body.date,
+				},
+			],
+		});
+    console.log(dm)
+    await User.updateOne({ _id: req.user._id }, { $push: { DMs: dm._id } });
+    await User.updateOne({ _id: req.body.userId }, { $push: { DMs: dm._id } });
     dm.save();
     res.send("saved");
+  }
+));
+
+// This adds a message to an ongoing DM/conversation
+router.post(
+	"/:id",
+	isLoggedInReject,
+	catchAsync(async (req, res) => {
+    const message = {
+      userId: req.user._id,
+      message: req.body.message,
+      date: req.body.date
+    };
+    const result = await DM.updateOne({_id: req.params.id}, {$push: {messages:message}});
+		console.log(result);
+
+		res.status(200).json(result);
+	})
+);
+
+// This retrieves a conversation
+router.get(
+  "/:id", isLoggedInReject,
+  catchAsync(async (req, res) => {
+    const dm = await DM.findOne({ _id: req.params.id });
+    res.send(dm);
   })
 );
 
-router.get(
-  "/:id",
-  catchAsync(async (req, res) => {
-    const user = await User.findOne({ _id: req.params.id });
-    res.send(user);
-  })
-);
-
-router.get(
-  "/private/:id",
-  catchAsync(async (req, res) => {
-    const user = await DM.findOne({ _id: req.params.id });
-    res.send(user);
-  })
-);
+// router.get(
+//   "/private/:id",
+//   catchAsync(async (req, res) => {
+//     const user = await DM.findOne({ _id: req.params.id });
+//     res.send(user);
+//   })
+// );
 
 module.exports = router;
