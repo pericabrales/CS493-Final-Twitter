8e8c7539112105d56bc3fe36c3408dfaabf1355d
matyevaa <59642740+matyevaa@users.noreply.github.com>
2021-06-07 06:09:03 -0700

    added tweets get by userId & post

--- models/tweet.js
+++ models/tweet.js
@@ -9,11 +9,12 @@ const tweetSchema = new mongoose.Schema({
 	},
 	message: {
 		type: String,
-		required: true,
+		required: 'true',
 		maxlength: 280,
 	},
 	favorites: [{ type: Schema.Types.ObjectId, ref: "User" }],
 });
 
 const Tweet = mongoose.model("Tweet", tweetSchema);
+
 module.exports = Tweet;
